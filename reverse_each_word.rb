ecc9b0918cdd869629a0591e679176e2c7160358
Malcom Fetus <sora.vista@gmail.com>
2020-02-22 05:21:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,8 @@ def reverse_each_word(phrase)
 	if new_phrase = []
 	phrase.split(" ").each do |word|
       	new_phrase << word.reverse
-  else new_phrase = []
+
+   new_phrase = []
   phrase.split(" ").collect do |word|
     new_phrase
 	end
