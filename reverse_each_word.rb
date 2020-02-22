da2e2900abe776fa801200053b6a3cae99565441
Malcom Fetus <sora.vista@gmail.com>
2020-02-22 05:11:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,11 @@
 require 'pry'
 def reverse_each_word(phrase)
-	new_phrase = []
+	if new_phrase = []
 	phrase.split(" ").each do |word|
       	new_phrase << word.reverse
+  else
+    phrase.collect
+"#{new_phrase}"
 	end
   new_phrase.join(" ")
 end
